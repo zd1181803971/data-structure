d83a47d0c6f7326a6c166be5aee3e18cd63d42d1
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2022-02-15 21:20:26 +0800

    选择排序

---
+++ DataStructers/src/com/dzu/sort/SelectSort.java
@@ -0,0 +1,36 @@
+package com.dzu.sort;
+
+/**
+ * @author by ZhaoDong
+ * @Classname SelectSort
+ * @Description 选择排序   相比 冒泡排序 速度更快
+ * @Date 2022/2/15 20:49
+ */
+public class SelectSort {
+    public static void main(String[] args) {
+
+        int[] arr = {7, 2, 5, 3, 1, 10, 9};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            int min = arr[i];
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            if (index != i) {
+                arr[index] = arr[i];
+                arr[i] = min;
+            }
+        }
+
+        System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.printf(arr[i] + " ");
+        }
+    }
+}
