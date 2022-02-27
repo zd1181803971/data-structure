684a6a01a28ac3711dd7e11f249b9862213edd8c
zhaodong <1181803971@qq.com>
2022-02-27 17:53:37 +0800

    快速排序

---
+++ DataStructers/src/com/dzu/sort/QuickSort.java
@@ -0,0 +1,51 @@
+package com.dzu.sort;
+
+/**
+ * @author ZhaoDong
+ * @date 2022/2/27 17:19
+ * @description 快速排序 (TODO
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {-9, 78, 0, 23, -567, 70};
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivot = arr[(left + right) / 2];
+
+        while (l < r) {
+            while (arr[l] < pivot) {
+                l += 1;
+            }
+            while (arr[r] > pivot) {
+                r -= 1;
+            }
+            if (l >= r) {
+                break;
+            }
+            int temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+
+            if (arr[l] == pivot) {
+                r -= 1;
+            }
+            if (arr[r] == pivot) {
+                l += 1;
+            }
+        }
+        if (l == r) {
+            l += 1;
+            r -= 1;
+        }
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        if (right > l) {
+            quickSort(arr, l, right);
+        }
+    }
+}
