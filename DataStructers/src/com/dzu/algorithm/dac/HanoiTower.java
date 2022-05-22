784bcbe660dbb170d3e78b46f3dcc5273af28c55
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2022-05-22 17:15:29 +0800

    p155 分治算法解决汉诺塔

--- DataStructers/src/com/dzu/algorithm/dac/HanoiTower.java
+++ DataStructers/src/com/dzu/algorithm/dac/HanoiTower.java
@@ -10,18 +10,18 @@ public class HanoiTower {
         hanoiTower(3, 'A', 'B', 'C');
     }
 
-    public static void hanoiTower(int num, char a, char b, char c) {
+    public static void hanoiTower(int num, char start, char temp, char end) {
         // 如果只有一个盘，A->C
         if (num == 1) {
-            System.out.println("第" + num + "个盘从" + a + "->" + c);
+            System.out.println("第" + num + "个盘从" + start + "->" + end);
         } else {
             // 如果n>=2个盘，我们总可以看作是两个盘 1、最下面的盘，2、上面的盘
             // 先把最上面的盘 A->B
-            hanoiTower(num - 1, a, c, b);
+            hanoiTower(num - 1, start, end, temp);
             // 最下面的盘 A-> C
-            System.out.println("第" + num + "个盘从" + a + "->" + c);
+            System.out.println("第" + num + "个盘从" + start + "->" + end);
             // 把B塔的所有的盘从 B->C
-            hanoiTower(num - 1, b, a, c);
+            hanoiTower(num - 1, temp, start, end);
         }
     }
 
